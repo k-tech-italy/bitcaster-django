2c9b00eb15d65fbafb70b60376a8c068d4724464
know46 <krzysiek.nooworyta@gmail.com>
2025-05-15 17:01:28 +0200

    added bitcaster user mixin

---
+++ tests/test_bitcaster_user_mixin.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+
+from bitcaster_django.mixins import BitcasterUserMixin
+
+
+def test_user_create(monkeypatch):
+    with patch('requests.request') as mocked_request:
+        bitcaster_user_mixin = BitcasterUserMixin(email='example@mail')
+
+        bitcaster_user_mixin.create()
+
+        mocked_request.assert_called_once_with('post', 'http://dummyhost/api/o/dummyorg/u/',
+                                               headers={'Authorization': 'Key dummytoken'},
+                                               timeout=15, data={'email': 'example@mail'})
+
+def test_user_delete(monkeypatch):
+    with patch('requests.request') as mocked_request:
+        bitcaster_user_mixin = BitcasterUserMixin(email='example@mail')
+
+        bitcaster_user_mixin.delete()
+
+        mocked_request.assert_called_once_with('delete', 'http://dummyhost/api/o/dummyorg/u/example@mail',
+                                               timeout=15, headers={'Authorization': 'Key dummytoken'})
